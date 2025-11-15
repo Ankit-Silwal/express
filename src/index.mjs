c45c085508b46be5530a82ea4024ab295b4702da
GubbinLubbin <silwalankit27@gmail.com>
2025-11-15 15:43:48 +0530

    Added the cookies and signed cookies in the project

--- src/index.mjs
+++ src/index.mjs
@@ -1,13 +1,16 @@
 import express from "express";
-import routes from "./routes/users.mjs";
+import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(express.json());
+app.use(cookieParser('helloworld'));
 app.use(routes);
 app.listen(PORT, () => {
   console.log(`The server has begun at PORT number:${PORT}`);
 });
 app.get("/", (req, res) => {
+  res.cookie('hello','world',{maxAge:30000,signed:true})
   res.status(201).json({ msg: "The server has begun yeah bitchhh" });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -1,10 +1,15 @@
-import {Router} from "express";
+import { Router} from "express";
 const router=Router();
 router.get("/api/products",(req,res)=>{
-  res.send([{
+  console.log(req.headers.cookie);
+  console.log(req.cookies)
+  if(req.signedCookies.hello&&req.signedCookies.hello==='world'){
+    return res.send([{
     id:123,
     name:"Chicken Breast",
     price:12.99,
   }]);
+  }
+  return res.send({msg:"Sorry you need the correct cookie"});
 })
 export default router;
